cea00273fd7b3bd2d6d6e00c57c0a22e37ba970a
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-07-31 20:05:24 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,7 +6,6 @@ FROM Table_fruit_basket
 LEFT JOIN Table_fridge_contents
 ON Table_fruit_basket.Item = Table_fridge_contents.Item;
 
-
 --Inner join to show which items in fruit basket need to be kept in fridge
 SELECT Table_fruit_basket.Item, Table_fruit_basket.Quantity, Table_fruit_basket.PricePerItem, Table_fruit_basket.UseBy
 FROM Table_fruit_basket
@@ -16,3 +15,8 @@ ON Table_fruit_basket.Item = Table_fridge_contents.Item;
 --Query applying stored function (see tables.sql) to find total cost of items in Table_fruit_basket (quantity * price per item)
 SELECT Item, Quantity, PricePerItem, stock_kitchen.TotalPrice(Quantity, PricePerItem) AS TotalPrice
 FROM Table_fruit_basket;
+
+--Query with sub-query to determine items in particular table e.g. freezer that are high in stock
+SELECT Item, Quantity, PricePerItem, UseBy
+FROM Table_freezer_contents
+WHERE Quantity > (SELECT AVG(Quantity) FROM Table_freezer_contents);
